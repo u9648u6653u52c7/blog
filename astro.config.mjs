6a123525daaec00643822ae28880a9e1dce953a8
Theo <u9648u6653u52c7@gmail.com>
2025-01-10 16:57:37 +0800

    chore: github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://blog.momtaste.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
