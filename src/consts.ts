cb46e62e28af125a418dc1249165be40d86bdcc6
Theo <u9648u6653u52c7@gmail.com>
2025-01-13 11:34:52 +0800

    chore: add new posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Theo Chen",
+  EMAIL: "u9648u6653u52c7@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 2,
 };
 
 export const HOME: Metadata = {
@@ -31,14 +31,10 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   { 
     NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://x.com/_cvman",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/u9648u6653u52c7"
   },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
 ];
---
+++ src/content/blog/new-begin/index.md
@@ -0,0 +1,7 @@
+---
+title: 新的开始
+description: ""
+date: "2017-11-12 20:25:47"
+---
+
+![New Team](./team.jpeg)
